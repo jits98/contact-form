8f8168183457f70a9feda7844c014ac4a1bcd04b
jits98 <jits8809@gmail.com>
2024-12-13 19:10:39 +0530

    add a test case

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function submit() {
+  const inputElement = document.querySelector(".js-name");
 
+  const inputValue = inputElement.value;
+
+  if (!inputValue) {
+    document.querySelector(".js-blank-output").innerHTML =
+      "This field is required";
+  } else {
+    alert("Message sent");
+  }
+}
